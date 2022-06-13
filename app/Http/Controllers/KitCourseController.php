c7c46cebb0cc1c48ac0f1709e7784365348f2bc9
jocelinkisenga <kisengajocelin7@gmail.com>
2022-06-13 18:48:48 +0200

    adding kit relationship to course model

--- app/Http/Controllers/KitCourseController.php
+++ app/Http/Controllers/KitCourseController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KitCourse;
+use App\Models\Course;
 
 class KitCourseController extends Controller
 {
     //
     public function index(){
-        $
+        $kitcourses = KitCourse::all();
+        return response()->json(['kits'=>$kitcourses],200);
+    }
+
+    public function store (int $id){
+
+        $course = Course::find(2);
+
+        KitCourse::create([
+                'user_id'=>1,
+                'course_id'=>$course->id
+            ]);
+        return response()->json('success');
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,23 @@ use Illuminate\Support\Facades\Route;
     //return $request->user();
 //});
 
+
+//course routes
 Route::get('/course',[App\Http\Controllers\CourseController::class, 'index']);
 Route::post('/course',[App\Http\Controllers\CourseController::class, 'store']);
 Route::get('/course/{id}',[App\Http\Controllers\CourseController::class, 'show']);
 
+//chapter routes
 Route::get('/chapter',[App\Http\Controllers\ChapterController::class, 'index']);
 Route::post('/chapter',[App\Http\Controllers\ChapterController::class, 'store']);
 Route::get('/chapter/{id}',[App\Http\Controllers\ChapterController::class, 'show']);
-// routes/api.php
+
+// kit course routes
+Route::get('/kit',[App\Http\Controllers\KitCourseController::class, 'index']);
+Route::post('/kit/{id}',[App\Http\Controllers\KitCourseController::class, 'store']);
+
+// user routes
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
-
 Route::get('/user', [App\Http\Controllers\ProfileController::class, 'user_courses']);
-
 Route::post('/me', [App\Http\Controllers\AuthController::class, 'me'])->middleware('auth:sanctum');
