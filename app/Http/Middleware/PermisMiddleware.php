b5db1567c593f57ed187aaa1453362f238584605
jocelinkisenga <kisengajocelin7@gmail.com>
2022-06-23 15:35:48 +0200

    i just configured the route for adding course

--- app/Http/Middleware/PermisMiddleware.php
+++ app/Http/Middleware/PermisMiddleware.php
@@ -20,13 +20,10 @@ class PermisMiddleware
     public function handle(Request $request, Closure $next)
     {
 
+        
         $userId = Auth::user()->id;
-        $permi = new Permission();
-        $permis = $permi->first()->whereUser_id($userId)->get();
         
-
-        
-            if ($permis)
+            if ($this->user_permis() == $userId)
             {
                return $next($request);
             }
@@ -34,6 +31,19 @@ class PermisMiddleware
 
                 return response()->json('you are not allowed');
             }
+
+
         
     }
+
+    public function user_permis(){
+            $permi = new Permission();
+            $userId = Auth::user()->id;
+            $permis = $permi->first()->whereUser_id($userId)->get();
+                foreach($permis as $permission){
+                    $user_id = $permission->user_id;
+                    return $user_id;
+                }
+        
+        }
 }
